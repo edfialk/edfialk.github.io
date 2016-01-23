cd4c27054bab61de4470407e2a654b5beb3ee981
vagrant <edfialk@gmail.com>
2016-01-23 02:54:42 +0000

    init

---
+++ scripts/kloj.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var Link = require('react-router').Link;
+
+module.exports = React.createClass({
+	render() {
+	    return (
+	      <div><h3>Kloj</h3></div>
+	    )
+	}
+});
